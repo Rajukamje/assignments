159a03ab33aee6432b45a5a936629637eac2c768
Rajukamje <133796833+Rajukamje@users.noreply.github.com>
2023-08-18 18:34:06 +0530

    Add files via upload

---
+++ bigest_number_34.java
@@ -0,0 +1,18 @@
+public class bigest_number_34 {
+    public static void main(String[] args) {
+        int x,y,z;
+        x=34;
+        y=34;
+        z=0;
+
+        if (x>y){
+            System.out.println("x is bigest number"+x);
+        }
+        if (y>x){
+            System.out.println("y is biggest number"+y);
+        }
+        else {
+            System.out.println("invalid number=="+z);
+        }
+    }
+}
